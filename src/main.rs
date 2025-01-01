be195bd7b7705b1ac8f79a6b25c6f9064bc68ed2
Joshua Lucas <joshua@wholesalegorilla.net>
2024-12-31 22:33:35 -0500

    feat: add logic to count words in the book file

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,41 @@ fn main() {
     let book_string = fs::read_to_string(file_path);
 
     match book_string {
-        Ok(value) => println!("The book you chose is :{}", value),
+        Ok(value) => println!("There are {} words in {}.", word_count(value), title),
         Err(e) => println!("Error: {}", e),
+    }
+}
+
+// Counts to number of words in the given book.
+fn word_count(text: String) -> usize {
+    text.split_whitespace().count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_word_count() {
+        let test_cases = vec![
+            ("Testing Three Words", 3),
+            ("Testing Puncuation.", 2),
+            (" Testing whitespace ", 2),
+            (
+                "This is line one,
+                 This is line two.",
+                8,
+            ),
+            ("Testing New/n Test", 3),
+        ];
+        for (text, expected) in test_cases.iter() {
+            assert_eq!(
+                word_count(text.to_string()),
+                *expected,
+                "Test failed for input: '{}', expected: {}",
+                text,
+                expected
+            );
+        }
     }
 }
